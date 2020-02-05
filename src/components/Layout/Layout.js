0dfe8fb93ca4fdc0933292c822f16e5cf8ba903a
F05249D <F05249D@SBRNL1DT0082028>
2020-02-05 17:36:36 -0300

    Removed text from top

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import classes from './Layout.css'
 
 const layout = (props) => (
     <Aux>
-        <div>toolbar, sidedrawer, backdrop</div>
+        {/* <div>toolbar, sidedrawer, backdrop</div> */}
         <main className={classes.Content}>
             {props.children}
         </main>
