d0e0da4af0cd23a81eff7b1e359d496d3f887c40
F05249D <F05249D@SBRNL1DT0082028>
2020-02-05 15:01:53 -0300

    Calculating ingredient sum dinamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
 
-    const ingredients = Object.keys(props.ingredients).map(igKey => {
+    let ingredients = Object.keys(props.ingredients).map(igKey => {
+
         return [...Array(props.ingredients[igKey])].map((_, i) => {
+
            return <BurgerIngredient key={igKey + 1} type={igKey} />
+
         });
-    })
+
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, [])
+
+    if (ingredients.length === 0) {
+        ingredients = <p>Please start adding ingredients</p>    
+    }
 
     return (
         <div className={classes.Burger}>
