4970d52e75e7e6e9c9a48172a9c7c58909061b07
Joao <dp.joaopedro@gmail.com>
2020-05-10 09:56:00 -0300

    async loading component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from "../../../components/UI/Input/Input";
 import { connect } from "react-redux";
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
+import {updateObject } from '../../../shared/utility'
 class ContactData extends Component {
     state = {
         orderForm: {
@@ -135,21 +136,16 @@ class ContactData extends Component {
     };
 
     inputChangedHandler = (event, inputIdentifier) => {
-        const updateOrderForm = {
-            ...this.state.orderForm,
-        };
 
-        const updatedFormElement = {
-            ...updateOrderForm[inputIdentifier],
-        };
+        const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+            value: event.target.value,
+            valid:this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            touched: true
+        })
 
-        updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(
-            updatedFormElement.value,
-            updatedFormElement.validation
-        );
-        updatedFormElement.touched = true;
-        updateOrderForm[inputIdentifier] = updatedFormElement;
+        const updateOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier]: updatedFormElement
+        });
 
         let formIsValid = true;
 
